e423821297f9fb25a03f0fe493665b3832381f2f
kcy <ghkdua1829@naver.com>
2019-11-21 02:49:07 +0900

    지도 마커 표시 및 정보 띄우기

--- app/src/main/java/com/example/map_part/MapsActivity.java
+++ app/src/main/java/com/example/map_part/MapsActivity.java
@@ -2,18 +2,37 @@ package com.example.map_part;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMarkerClickListener,GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
+    Marker selectedMarker;
+    View marker_root_view;
+    TextView tv_marker,placeName,placeCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +42,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        placeName = findViewById(R.id.placeName);
+        placeCall = findViewById(R.id.placeCall);
+
     }
 
 
@@ -40,8 +63,121 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.537523, 126.96558), 14));
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnMapClickListener(this);
+        setCustomMarkerView();
+        getSampleMarkerItems();
+
     }
+
+    private void getSampleMarkerItems() {
+        ArrayList<MarkerItem> sampleList = new ArrayList();
+
+
+        sampleList.add(new MarkerItem(37.538523, 126.96568, "마음병원","02-970-4456"));
+        sampleList.add(new MarkerItem(37.527523, 126.96568, "아리랑병원","02-350-9316"));
+        sampleList.add(new MarkerItem(37.549523, 126.96568, "사랑병원","02-631-8553"));
+        sampleList.add(new MarkerItem(37.538523, 126.95768, "주작병원","02-766-3543"));
+
+
+        for (MarkerItem markerItem : sampleList) {
+            addMarker(markerItem, false);
+        }
+
+    }
+
+    private void setCustomMarkerView() {
+
+        marker_root_view = LayoutInflater.from(this).inflate(R.layout.custom_marker, null);
+        tv_marker = (TextView) marker_root_view.findViewById(R.id.tv_marker);
+    }
+
+    private Marker addMarker(MarkerItem markerItem, boolean isSelectedMarker) {
+
+
+        LatLng position = new LatLng(markerItem.getLat(), markerItem.getLon());
+        String name = markerItem.getName();
+        String call = markerItem.getCall();
+//        String formatted = NumberFormat.getCurrencyInstance().format((name));
+
+        tv_marker.setText(name);
+
+        if (isSelectedMarker) {
+            tv_marker.setBackgroundResource(R.drawable.ic_marker_phone_blue);
+            tv_marker.setTextColor(Color.WHITE);
+        } else {
+            tv_marker.setBackgroundResource(R.drawable.ic_marker_phone);
+            tv_marker.setTextColor(Color.BLACK);
+        }
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title(name);
+        markerOptions.position(position);
+        markerOptions.snippet(call);
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(createDrawableFromView(this, marker_root_view)));
+
+
+        return mMap.addMarker(markerOptions);
+
+    }
+
+    // View를 Bitmap으로 변환
+    private Bitmap createDrawableFromView(Context context, View view) {
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        view.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+
+        return bitmap;
+    }
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        CameraUpdate center = CameraUpdateFactory.newLatLng(marker.getPosition());
+        placeName.setText(marker.getTitle());
+        placeCall.setText(marker.getSnippet());
+        mMap.animateCamera(center);
+        changeSelectedMarker(marker);
+        return true;
+    }
+
+    private void changeSelectedMarker(Marker marker) {
+        // 선택했던 마커 되돌리기
+        if (selectedMarker != null) {
+            addMarker(selectedMarker, false);
+            selectedMarker.remove();
+        }
+
+        // 선택한 마커 표시
+        if (marker != null) {
+            selectedMarker = addMarker(marker, true);
+            marker.remove();
+        }
+
+
+    }
+
+    private Marker addMarker(Marker marker, boolean isSelectedMarker) {
+        double lat = marker.getPosition().latitude;
+        double lon = marker.getPosition().longitude;
+        String name = marker.getTitle();
+        String call = marker.getSnippet();
+        MarkerItem temp = new MarkerItem(lat, lon,name,call );
+        return addMarker(temp, isSelectedMarker);
+
+    }
+    @Override public void onMapClick(LatLng latLng) {
+        changeSelectedMarker(null);
+    }
+
+
 }
+
---
+++ app/src/main/java/com/example/map_part/MarkerItem.java
@@ -0,0 +1,47 @@
+package com.example.map_part;
+
+public class MarkerItem {
+    double lat;
+    double lon;
+    String name;
+    String call;
+
+    public MarkerItem(double lat,double lon,String name,String call) {
+        this.lat = lat;
+        this.lon = lon;
+        this.name = name;
+        this.call = call;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCall() {
+        return call;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCall(String call) {
+        this.call = call;
+    }
+}
