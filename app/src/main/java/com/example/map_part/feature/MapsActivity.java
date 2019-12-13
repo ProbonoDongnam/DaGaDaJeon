20173dfde373b66782778aec8a899beef1c9bd69
kcy <ghkdua1829@naver.com>
2019-12-13 23:24:08 +0900

    code review 추가

--- app/src/main/java/com/example/map_part/feature/MapsActivity.java
+++ app/src/main/java/com/example/map_part/feature/MapsActivity.java
@@ -61,12 +61,12 @@ import noman.googleplaces.PlaceType;
 import noman.googleplaces.PlacesException;
 import noman.googleplaces.PlacesListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMarkerClickListener,GoogleMap.OnMapClickListener, PlacesListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMapClickListener, PlacesListener {
 
     private GoogleMap mMap;
     Marker selectedMarker;
     View marker_root_view;
-    TextView tv_marker,placeName,placeCall;
+    TextView tv_marker, placeName, placeCall;
     List<Marker> previous_marker = null;
 
     private Marker currentMarker = null;
@@ -79,7 +79,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int PERMISSIONS_REQUEST_CODE = 100;
     boolean needRequest = false;
 
-    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};  // 외부 저장소
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};  // 외부 저장소
 
 
     Location mCurrentLocatiion;
@@ -92,6 +92,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     private View mLayout;  // Snackbar 사용하기 위해서는 View가 필요합니다.
+
     // (참고로 Toast에서는 Context가 필요했습니다.)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -138,10 +139,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         ArrayList<MarkerItem> sampleList = new ArrayList();
 
 
-        sampleList.add(new MarkerItem(37.538523, 126.96568, "A Bệnh viện","02-970-4456"));
-        sampleList.add(new MarkerItem(37.527523, 126.96568, "B Bệnh viện","02-350-9316"));
-        sampleList.add(new MarkerItem(37.549523, 126.96568, "C Bệnh viện","02-631-8553"));
-        sampleList.add(new MarkerItem(37.538523, 126.95768, "D Bệnh viện","02-766-3543"));
+        sampleList.add(new MarkerItem(37.538523, 126.96568, "A Bệnh viện", "02-970-4456"));
+        sampleList.add(new MarkerItem(37.527523, 126.96568, "B Bệnh viện", "02-350-9316"));
+        sampleList.add(new MarkerItem(37.549523, 126.96568, "C Bệnh viện", "02-631-8553"));
+        sampleList.add(new MarkerItem(37.538523, 126.95768, "D Bệnh viện", "02-766-3543"));
 
 
         for (MarkerItem markerItem : sampleList) {
@@ -150,10 +151,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    //커스텀 마커 모양 inflate
+    //커스텀 마커 모양 inflate (바꿔준다)
     private void setCustomMarkerView() {
         marker_root_view = LayoutInflater.from(this).inflate(R.layout.custom_marker, null);
         tv_marker = (TextView) marker_root_view.findViewById(R.id.tv_marker);
+    }
+
+
+    // View를 Bitmap으로 변환
+    private Bitmap createDrawableFromView(Context context, View view) {
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        view.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+
+        return bitmap;
+    }
+
+    //마커가 클릭 됐을 때
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        CameraUpdate center = CameraUpdateFactory.newLatLng(marker.getPosition());
+        placeName.setText(marker.getTitle());
+        placeCall.setText(marker.getSnippet());
+        mMap.animateCamera(center);
+        changeSelectedMarker(marker);
+        return true;
+    }
+
+    //선택된 마커를 바꿔주는 메소드
+    private void changeSelectedMarker(Marker marker) {
+        // 선택했던 마커 되돌리기
+        if (selectedMarker != null) {
+            addMarker(selectedMarker, false);
+            selectedMarker.remove();
+        }
+
+        // 선택한 마커 표시
+        if (marker != null) {
+            selectedMarker = addMarker(marker, true);
+            marker.remove();
+        }
+
+
+    }
+
+    //마커를 추가해주는 메소드
+    private Marker addMarker(Marker marker, boolean isSelectedMarker) {
+        double lat = marker.getPosition().latitude;
+        double lon = marker.getPosition().longitude;
+        String name = marker.getTitle();
+        String call = marker.getSnippet();
+        MarkerItem temp = new MarkerItem(lat, lon, name, call);
+        return addMarker(temp, isSelectedMarker);
+
     }
 
     //커스텀 마커를 추가하는 메소드.
@@ -166,7 +225,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         tv_marker.setText(name);
 
-        //마커가 선택 되었을 때 색깔을 바꿔주는 분기
+        //마커가 선택 되었을 때 색깔을 바꿔주는 분기문
         if (isSelectedMarker) {
             tv_marker.setBackgroundResource(R.drawable.ic_marker_phone_blue);
             tv_marker.setTextColor(Color.WHITE);
@@ -185,59 +244,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    // View를 Bitmap으로 변환
-    private Bitmap createDrawableFromView(Context context, View view) {
-
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        view.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
-        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
-        view.buildDrawingCache();
-        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-
-        Canvas canvas = new Canvas(bitmap);
-        view.draw(canvas);
-
-        return bitmap;
-    }
+    //맵을 클릭하면 실행하는 함수가 아닌듯??
     @Override
-    public boolean onMarkerClick(Marker marker) {
-
-        CameraUpdate center = CameraUpdateFactory.newLatLng(marker.getPosition());
-        placeName.setText(marker.getTitle());
-        placeCall.setText(marker.getSnippet());
-        mMap.animateCamera(center);
-        changeSelectedMarker(marker);
-        return true;
-    }
-
-    private void changeSelectedMarker(Marker marker) {
-        // 선택했던 마커 되돌리기
-        if (selectedMarker != null) {
-            addMarker(selectedMarker, false);
-            selectedMarker.remove();
-        }
-
-        // 선택한 마커 표시
-        if (marker != null) {
-            selectedMarker = addMarker(marker, true);
-            marker.remove();
-        }
-
-
-    }
-
-    private Marker addMarker(Marker marker, boolean isSelectedMarker) {
-        double lat = marker.getPosition().latitude;
-        double lon = marker.getPosition().longitude;
-        String name = marker.getTitle();
-        String call = marker.getSnippet();
-        MarkerItem temp = new MarkerItem(lat, lon,name,call );
-        return addMarker(temp, isSelectedMarker);
-
-    }
-    @Override public void onMapClick(LatLng latLng) {
+    public void onMapClick(LatLng latLng) {
         changeSelectedMarker(null);
     }
 
@@ -288,8 +297,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    public void showPlaceInformation(LatLng location)
-    {
+    public void showPlaceInformation(LatLng location) {
         mMap.clear();//지도 클리어
 
         if (previous_marker != null)
@@ -319,7 +327,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setDefaultLocation();
 
 
-
         //런타임 퍼미션 처리
         // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
         int hasFineLocationPermission = ContextCompat.checkSelfPermission(this,
@@ -328,9 +335,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Manifest.permission.ACCESS_COARSE_LOCATION);
 
 
-
         if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
-                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED   ) {
+                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
 
             // 2. 이미 퍼미션을 가지고 있다면
             // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
@@ -339,7 +345,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             startLocationUpdates(); // 3. 위치 업데이트 시작
 
 
-        }else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
 
             // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
@@ -352,7 +358,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     public void onClick(View view) {
 
                         // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
-                        ActivityCompat.requestPermissions( MapsActivity.this, REQUIRED_PERMISSIONS,
+                        ActivityCompat.requestPermissions(MapsActivity.this, REQUIRED_PERMISSIONS,
                                 PERMISSIONS_REQUEST_CODE);
                     }
                 }).show();
@@ -361,12 +367,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             } else {
                 // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
                 // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
-                ActivityCompat.requestPermissions( this, REQUIRED_PERMISSIONS,
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
                         PERMISSIONS_REQUEST_CODE);
             }
 
         }
-
 
 
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
@@ -376,7 +381,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onMapClick(LatLng latLng) {
 
-                Log.d( TAG, "onMapClick :");
+                Log.d(TAG, "onMapClick :");
             }
         });
     }
@@ -415,14 +420,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     };
 
 
-
     private void startLocationUpdates() {
 
         if (!checkLocationServicesStatus()) {
 
             Log.d(TAG, "startLocationUpdates : call showDialogForLocationServiceSetting");
             showDialogForLocationServiceSetting();
-        }else {
+        } else {
 
             int hasFineLocationPermission = ContextCompat.checkSelfPermission(this,
                     Manifest.permission.ACCESS_FINE_LOCATION);
@@ -430,9 +434,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     Manifest.permission.ACCESS_COARSE_LOCATION);
 
 
-
             if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED ||
-                    hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED   ) {
+                    hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
 
                 Log.d(TAG, "startLocationUpdates : 퍼미션 안가지고 있음");
                 return;
@@ -462,7 +465,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Log.d(TAG, "onStart : call mFusedLocationClient.requestLocationUpdates");
             mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
 
-            if (mMap!=null)
+            if (mMap != null)
                 mMap.setMyLocationEnabled(true);
 
         }
@@ -482,8 +485,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mFusedLocationClient.removeLocationUpdates(locationCallback);
         }
     }
-
-
 
 
     public String getCurrentAddress(LatLng latlng) {
@@ -551,7 +552,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-
+    //지도의 초기위치를 서울로 이동
     public void setDefaultLocation() {
 
 
@@ -586,16 +587,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Manifest.permission.ACCESS_COARSE_LOCATION);
 
 
-
         if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
-                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED   ) {
+                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
             return true;
         }
 
         return false;
 
     }
-
 
 
     /*
@@ -606,7 +605,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                            @NonNull String[] permissions,
                                            @NonNull int[] grandResults) {
 
-        if ( permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
 
             // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
 
@@ -623,12 +622,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
 
 
-            if ( check_result ) {
+            if (check_result) {
 
                 // 퍼미션을 허용했다면 위치 업데이트를 시작합니다.
                 startLocationUpdates();
-            }
-            else {
+            } else {
                 // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
 
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
@@ -646,7 +644,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         }
                     }).show();
 
-                }else {
+                } else {
 
 
                     // "다시 묻지 않음"을 사용자가 체크하고 거부를 선택한 경우에는 설정(앱 정보)에서 퍼미션을 허용해야 앱을 사용할 수 있습니다.
